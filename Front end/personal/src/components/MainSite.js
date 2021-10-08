f48611964f7600ec1008f3eb80f0ba283c25439b
Philipsen,Sam S <s.philipsen@student.fontys.nl>
2021-10-08 22:05:39 +0200

    Updated React to display the coin toss. Does not actually update the points because I could not figure it out.

--- Front end/personal/src/components/MainSite.js
+++ Front end/personal/src/components/MainSite.js
@@ -5,8 +5,11 @@ const client = axios.create({
     baseURL: "http://localhost:8080/users?name=Peter"
 });
 
+let loggedIn = false;
+
 const MainSite = () => {
     const [user, setUser] = useState([]);
+    const [winText, setWinText] = useState("");
 
     useEffect( () => {
     }, []);
@@ -17,14 +20,42 @@ const MainSite = () => {
             setUser(response.data);
         }
         fetchData();
+        loggedIn = true;
+    }
+
+    //Could not get this to work.
+    /*const gameResultHandling = () => {
+        async function sendData() {
+            const response = await axios.post("http://localhost:8080/users", {user})
+        }
+    }*/
+
+    const handleCointoss = () => {
+        let coinSide;
+
+        let resultNumber = Math.floor(Math.random() * 2);
+        if(resultNumber === 0) {
+            coinSide = "heads";
+            setWinText("You win! " + coinSide)
+        } else {
+            coinSide = "tails";
+            setWinText("You lose! " + coinSide)
+        }
+    }
+
+    let loginButtonView = {}
+    let coinTossView = {}
+
+    if(loggedIn) {
+        loginButtonView.display = "none"
+    } else {
+        coinTossView.display = "none"
     }
 
     if(!user) return null
 
     return (
         <div>
-
-            {console.log(user)}
             {user.map(data => (
                 <h1>Welcome, {data.name}</h1>
                 ))}
@@ -34,9 +65,16 @@ const MainSite = () => {
             {user.map(data => (
                 <h2>Points: {data.points}</h2>
             ))}
-            <button onClick={handleLogin}>
+            <button onClick={handleLogin} style={loginButtonView}>
                 Log in
             </button>
+            <div className="coinTossCointainer" style={coinTossView}>
+                <p>Default coin side is <b>Heads</b>. Win = +50 points, lose = -50 points</p>
+                <button onClick={handleCointoss}>
+                    Flip coin
+                </button>
+                <p>{winText}</p>
+            </div>
         </div>
     );
 }
