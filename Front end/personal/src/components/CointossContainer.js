04a29607471f82cc99cd9434674b2dca9483f141
Philipsen,Sam S <s.philipsen@student.fontys.nl>
2021-10-29 16:09:02 +0200

    Added input for bet amount and coin side. Also added some more styling to the cointoss container

--- Front end/personal/src/components/CointossContainer.js
+++ Front end/personal/src/components/CointossContainer.js
@@ -1,42 +1,89 @@
 import React, {useEffect, useState} from "react"
+import "./cointoss-styles.css"
 
 const CointossContainer = props => {
     const [user, setUser] = useState(props.loggedInUser);
+    const [betAmount, setBetAmount] = useState();
+    const [coinSide, setCoinSide] = useState("Heads");
     const [winText, setWinText] = useState("");
+    const [winningSideText, setWinningSideText] = useState("");
 
     useEffect(() => {
         setUser(props.loggedInUser);
     }, [props.loggedInUser])
 
-    const handleCointoss = () => {
-        let coinSide;
+    const handleCointoss = e => {
+        e.preventDefault()
         var newPoints = 0;
+        var sides = ['Heads', 'Tails'];
 
-        if(user.points > 0) {
-            let resultNumber = Math.floor(Math.random() * 2);
-            if(resultNumber === 0) {
-                coinSide = "heads";
-                newPoints = user.points + 50;
-                setWinText("You win! " + coinSide)
-            } else {
-                coinSide = "tails";
-                newPoints = user.points - 50;
-                setWinText("You lose! " + coinSide)
+        if(typeof betAmount == "number") {
+            if(user.points > 0 && user.points - betAmount > 0) {
+                let result = sides[Math.floor(Math.random()*sides.length)]
+
+                if(result === coinSide) {
+                    newPoints = user.points + betAmount;
+                    setWinText("You win! You receive " + betAmount + " points. Your new balance is: " + newPoints + "!")
+                } else {
+                    newPoints = user.points - betAmount;
+                    setWinText("You lose! You lost " + betAmount + " points. Your new balance is: " + newPoints + "!")
+                }
+                setWinningSideText("The winning side is: " + result)
+                props.gamePointsChanged(newPoints);
             }
-            props.gamePointsChanged(newPoints);
+            else {
+                setWinText("You do not have sufficient points to play this game! (You have " + user.points + ", you need at least " + (betAmount + 1) + " to play.)")
+            }
+        } else {
+            alert("Input can only be a number!");
         }
-        else {
-            setWinText("You do not have sufficient points to play this game! (You have " + user.points + ", you need 50 to play.)")
-        }
+    }
 
+    const onBetChange = e => {
+        var amount = parseInt(e.target.value)
+        if (!isNaN(amount)) {
+            setBetAmount(parseInt(amount))
+        } else {
+            setBetAmount("");
+        }
+    }
+
+    const rbChanged = e => {
+        setCoinSide(e.target.value)
     }
 
     return (
         <div className="coinTossContainer">
-            <p>Default coin side is <b>Heads</b>. Win = +50 points, lose = -50 points</p>
-            <button onClick={handleCointoss}>
-                Flip coin
-            </button>
+            <form onSubmit={handleCointoss} className="coinTossForm">
+                <input
+                    type="text"
+                    className="bet-input"
+                    placeholder="Points you want to bet"
+                    value={betAmount}
+                    name="bet-points"
+                    onChange={onBetChange}
+                />
+                <input
+                    type="radio"
+                    value="Heads"
+                    name="bet-coinside"
+                    checked={coinSide === "Heads"}
+                    onChange={rbChanged}
+                />
+                Heads
+                <input
+                    type="radio"
+                    value="Tails"
+                    name="bet-coinside"
+                    checked={coinSide === "Tails"}
+                    onChange={rbChanged}
+                />
+                Tails
+                <button input="submit" className="bet-submit">
+                    Flip
+                </button>
+            </form>
+            <p>{winningSideText}</p>
             <p>{winText}</p>
         </div>
     )
--- Front end/personal/src/pages/login.js
+++ Front end/personal/src/pages/login.js
@@ -1,22 +1,32 @@
-import React from "react"
+import React, { useState } from "react"
 import { useHistory } from "react-router-dom";
+import "./login-styles.css"
 
 import axios from "axios";
 
 const Login = () => {
+    const [errorText, setErrorText] = useState("");
     const history = useHistory();
 
     const handleLogin = () => {
         async function fetchData() {
-            const response = await axios.get("http://localhost:8080/users?name=Peter");
-            localStorage.setItem('loggedInUser', JSON.stringify(response.data[0]));
-            history.push("/menu");
+            try {
+                const response = await axios.get("http://localhost:8080/users?name=Peter");
+                localStorage.setItem('loggedInUser', JSON.stringify(response.data[0]));
+                history.push("/menu");
+            } catch (error) {
+                setErrorText("Could not establish a connection with the API!");
+            }
+
         }
         fetchData();
     }
 
     return (
         <div className="loginContainer">
+            <h1 className="network-error-text">
+                {errorText}
+            </h1>
             <button onClick={handleLogin} id="loginButton">
                 Log in
             </button>
